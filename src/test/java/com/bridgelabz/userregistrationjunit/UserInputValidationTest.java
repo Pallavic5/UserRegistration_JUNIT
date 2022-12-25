ad6521a40a21c70ef48474eecf328177f394c5a0
Pallavic5 <pchedge235@gmail.com>
2022-12-25 20:36:59 +0530

    Added: Logic of Ability to user need to enter a valid first name.

---
+++ src/test/java/com/bridgelabz/userregistrationjunit/UserInputValidationTest.java
@@ -0,0 +1,55 @@
+package com.bridgelabz.userregistrationjunit;
+/*
+ * Problem Statement- UC 1 As a User need to enter a valid First Name
+- First name starts with Cap and has minimum 3 characters
+ */
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class UserInputValidationTest {
+	/*
+	 * declare the class object static
+	 */
+	static UserInputValidation input = null;
+
+	/*
+	 * In the BeforeClass and AfterClass we can initialize the class object, this
+	 * both classes are only called once and there method should be static.
+	 */
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("Before Class");
+		input = new UserInputValidation(); // initialize object.
+	}
+	/*
+	 * Before and After method run for each test cases.
+	 */
+	@Before
+	public void before() {
+		System.out.println("Before");
+	}
+	@Test
+	public void testFirstNameStartsWithCapitalLetterAndHasWithMinimumThreeCharacters_thenHappy() {
+		System.out.println("First");
+		assertTrue(input.isValidFirstName("Pallavi"));
+	}
+	@Test
+	public void testFirstNameDoesntStartWithCapitalLetterAndDontHaveMinimumThreeCharacters_thenSad() {
+		System.out.println("Second");
+		assertTrue(input.isValidFirstName("pallavi"));
+	}
+	@After
+	public void after() {
+		System.out.println("After");
+	}
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("After Class");
+		input = new UserInputValidation(); // initialize object.
+	}
+
+}
