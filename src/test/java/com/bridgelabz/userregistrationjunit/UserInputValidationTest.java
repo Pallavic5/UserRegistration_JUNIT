da68307637f9f5140e28e01e53c3c658c66e53d8
Pallavic5 <pchedge235@gmail.com>
2022-12-26 00:33:23 +0530

    Added: Logic of Ability to user need to enter a valid email.

--- src/test/java/com/bridgelabz/userregistrationjunit/UserInputValidationTest.java
+++ src/test/java/com/bridgelabz/userregistrationjunit/UserInputValidationTest.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.userregistrationjunit;
 /*
- * Problem Statement- UC 2 As a User need to enter a valid Last Name
-- Last name starts with Cap and has minimum 3 characters
+ * Problem Statement- UC 3 As a User need to enter a valid Email
+- E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl & co) 
+and 2 optional (xyz & in) with precise @ and . positions
  */
 import static org.junit.Assert.*;
 import org.junit.After;
@@ -15,7 +16,6 @@ public class UserInputValidationTest {
 	 * declare the class object static
 	 */
 	static UserInputValidation input = null;
-
 	/*
 	 * In the BeforeClass and AfterClass we can initialize the class object, this
 	 * both classes are only called once and there method should be static.
@@ -25,7 +25,6 @@ public class UserInputValidationTest {
 		System.out.println("Before Class");
 		input = new UserInputValidation(); // initialize object.
 	}
-
 	/*
 	 * Before and After method run for each test cases.
 	 */
@@ -33,6 +32,9 @@ public class UserInputValidationTest {
 	public void before() {
 		System.out.println("Before");
 	}
+	/*
+	 * FirstName Happy and Sad TestCase
+	 */
 	@Test
 	public void testFirstNameStartsWithCapitalLetterAndHasWithMinimumThreeCharacters_thenHappy() {
 		System.out.println("First");
@@ -43,19 +45,33 @@ public class UserInputValidationTest {
 		System.out.println("Second");
 		assertTrue(input.isValidFirstName("pallavi"));
 	}
+	/*
+	 * LastName Happy and Sad TestCase
+	 */
 	@Test
 	public void testLastNameStartsWithCapitalLetterAndHasWithMinimumThreeCharacters_thenHappy() {
 		assertTrue(input.isValidLastName("Punewar"));
 	}
-
 	@Test
 	public void testLastNameDoesntStartWithCapitalLetterAndDontHaveMinimumThreeCharacters_thenSad() {
 		assertFalse(input.isValidLastName("punewar"));
+	}
+	/*
+	 * Email Happy and Sad TestCase
+	 */
+	@Test
+	public void testEmailContainsRequiredCharacter_thenHappy() {
+		assertTrue(input.isValidEmail("abc.xyz@bl.co.in"));
+	}
+	@Test
+	public void testEmailDoestContainRequiredCharacter_thenSad() {
+		assertFalse(input.isValidEmail("Abc.@bl.co.src.in"));
 	}
 	@After
 	public void after() {
 		System.out.println("After");
 	}
+
 	@AfterClass
 	public static void afterClass() {
 		System.out.println("After Class");
