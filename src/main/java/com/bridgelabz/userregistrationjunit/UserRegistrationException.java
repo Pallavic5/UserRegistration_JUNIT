e4cf0267ad087dd6ea8a116fdf5118de123a6659
Pallavic5 <pchedge235@gmail.com>
2022-12-29 13:11:46 +0530

    Added: Refactor the Code to throw custom exceptions in case of Invalid User Details.

---
+++ src/main/java/com/bridgelabz/userregistrationjunit/UserRegistrationException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.userregistrationjunit;
+
+public class UserRegistrationException extends Exception {
+	public UserRegistrationException(String message) {
+		super(message);
+	}
+}
